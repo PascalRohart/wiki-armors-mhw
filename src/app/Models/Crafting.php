a5b7ffcb4234ee46019a7d85a50ea8fdf8b2484f
Pascal Rohart <pascalrohart3@orange.fr>
2023-11-03 13:52:38 +0100

    test

--- src/app/Models/Crafting.php
+++ src/app/Models/Crafting.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Crafting extends Model
 {
     use HasFactory;
+
+    //$table->foreign('id_materials_list_to_craft')->references('id')->on('materials_lists_to_craft');
+
+    public function materials_list_to_craft(): BelongsTo {
+        return $this->belongsTo(MaterialsListToCraft::class);
+    }
 }
